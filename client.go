fa831740d700a19079fce0858b3b8e46dc4d1f76
abin.thomas <abin.thomas@ng-voice.com>
2023-03-20 15:51:56 +0100

    chore: test listchartrepo

--- client.go
+++ client.go
@@ -218,16 +218,11 @@ func (c *HelmClient) ListChartRepo(entry repo.Entry) (string, error) {
 	}
 
 	chartRepo.CachePath = c.Settings.RepositoryCache
-	fmt.Println(chartRepo.CachePath)
 	str := chartRepo.CachePath + "/" + chartRepo.Config.Name + "-charts.txt"
-	fmt.Println("Before cat")
-	fmt.Println("Running /bin/sh -c cat " + str)
-	output, _ := exec.Command("/bin/sh", "-c", "cat "+str).Output()
-	fmt.Println("After cat")
-	fmt.Println(string(output))
+	output, err := exec.Command("/bin/sh", "-c", "cat "+str).Output()
 	if err == nil {
-		fmt.Println(str)
-		return string(output), nil
+		op := string(output)
+		return op, nil
 	} else {
 		return "Some Error", err
 	}
