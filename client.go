634e3aadcea7a447c4e160e94ca420dac39bd9d8
abin.thomas <abin.thomas@ng-voice.com>
2023-06-02 16:34:12 +0200

    chore(go-helm-client): test recursive dependencies update build

--- client.go
+++ client.go
@@ -980,30 +980,30 @@ func updateRecursiveDependencies(helmChart *chart.Chart, chartPathOptions *actio
 				dependency = nil
 
 			}
-			if err := action.CheckDependencies(helmChart, dependency); err != nil {
-				if dependencyUpdate {
-					man := &downloader.Manager{
-						ChartPath:        chartPath,
-						Keyring:          chartPathOptions.Keyring,
-						SkipUpdate:       false,
-						Getters:          c.Providers,
-						RepositoryConfig: c.Settings.RepositoryConfig,
-						RepositoryCache:  c.Settings.RepositoryCache,
-						Out:              c.output,
-					}
-					if err := man.Update(); err != nil {
-						return nil, err
-					}
+			// if err := action.CheckDependencies(helmChart, dependency); err != nil {
+			// 	if dependencyUpdate {
+			// 		man := &downloader.Manager{
+			// 			ChartPath:        chartPath,
+			// 			Keyring:          chartPathOptions.Keyring,
+			// 			SkipUpdate:       false,
+			// 			Getters:          c.Providers,
+			// 			RepositoryConfig: c.Settings.RepositoryConfig,
+			// 			RepositoryCache:  c.Settings.RepositoryCache,
+			// 			Out:              c.output,
+			// 		}
+			// 		if err := man.Update(); err != nil {
+			// 			return nil, err
+			// 		}
 
-					helmChart, _, err = c.GetChart(spec.ChartName, chartPathOptions)
-					if err != nil {
-						return nil, err
-					}
+			// 		helmChart, _, err = c.GetChart(spec.ChartName, chartPathOptions)
+			// 		if err != nil {
+			// 			return nil, err
+			// 		}
 
-				} else {
-					return nil, err
-				}
-			}
+			// 	} else {
+			// 		return nil, err
+			// 	}
+			// }
 		}
 	} else {
 		fmt.Println(helmChart.Metadata.Name + "<-- returning this chart")
